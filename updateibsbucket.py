4cd3ff7f9091da00e99ebc7ec5df6477158c3f60
benofben <ben.lackey@outlook.com>
2008-08-08 17:35:04 -0600

    broken

--- updateibsbucket.py
+++ updateibsbucket.py
@@ -1,7 +1,6 @@
 import quotes
 
-symbols=['C','CS','DB','GS','LEH','MER','RY','UBS','WB', 'HBC']
-# 'HBC','AIG'
+symbols=['C','CS','DB','GS','LEH','MER','RY','UBS','WB','HBC']#'JPM','BAC','WFC','WM','AIG']
 
 for symbol in symbols:
     quotes.downloadQuotes(symbol)
