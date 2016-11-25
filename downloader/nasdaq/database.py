41e1ec541c13d3b97e942ff4eddbffdc14021ebb
benofben <ben.lackey@outlook.com>
2016-11-24 23:10:04 -0800

    print

--- downloader/nasdaq/database.py
+++ downloader/nasdaq/database.py
@@ -51,5 +51,6 @@ class database():
         [rows, unused_total_rows, unused_page_token] = query_results.fetch_data()
         symbols = []
         for row in rows:
+            print(row)
             symbols.append(row[0])
         return symbols
