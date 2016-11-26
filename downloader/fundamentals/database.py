d625e12564a1aa05ca3d6c1580b2f9ca2f74836b
benofben <ben.lackey@outlook.com>
2016-11-26 13:15:46 -0800

    punctuation

--- downloader/fundamentals/database.py
+++ downloader/fundamentals/database.py
@@ -11,14 +11,14 @@ class database():
         self.dataset = self.client.dataset('downloader')
         schema = (
             bigquery.table.SchemaField(name='Symbol', field_type='STRING'),
-            bigquery.table.SchemaField(name='DownloadDate', field_type='DATE'),
+            bigquery.table.SchemaField(name='Date', field_type='DATE'),
             bigquery.table.SchemaField(name='Open', field_type='FLOAT'),
             bigquery.table.SchemaField(name='High', field_type='FLOAT'),
             bigquery.table.SchemaField(name='Low', field_type='FLOAT'),
             bigquery.table.SchemaField(name='Close', field_type='FLOAT'),
             bigquery.table.SchemaField(name='Shares', field_type='INTEGER'),
-            bigquery.table.SchemaField(name='EPS', field_type='FLOAT')
-            bigquery.table.SchemaField(name='InstitutionalOwnership', field_type='FLOAT')
+            bigquery.table.SchemaField(name='EPS', field_type='FLOAT'),
+            bigquery.table.SchemaField(name='InstitutionalOwnership', field_type='FLOAT'),
             bigquery.table.SchemaField(name='Dividend', field_type='FLOAT')
         )
         self.table = self.dataset.table('fundamentals', schema)
