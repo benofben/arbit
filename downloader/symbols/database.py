488e1f4ee673919bc5285b6eeaded40257ffaea3
benofben <ben.lackey@outlook.com>
2016-11-25 19:08:04 -0800

    more error handling

--- downloader/symbols/database.py
+++ downloader/symbols/database.py
@@ -8,8 +8,8 @@ class database():
         self.client = bigquery.Client()
 
 
-    def setSchema(self, table):
-        table.schema = (
+    def getSchema(self, table):
+        schema = (
             bigquery.table.SchemaField(name='Exchange', field_type='STRING'),
             bigquery.table.SchemaField(name='Symbol', field_type='STRING'),
             bigquery.table.SchemaField(name='Name', field_type='STRING'),
@@ -19,25 +19,28 @@ class database():
             bigquery.table.SchemaField(name='Sector', field_type='STRING'),
             bigquery.table.SchemaField(name='Industry', field_type='STRING')
         )
-        return table
+        return schema
 
 
     def create(self):
-        table = self.client.dataset('downloader').table('symbols')
-        if not table.exists():
-            table = self.setSchema(table)
-            table.create()
+        dataset = self.client.dataset('downloader')
+        table = dataset.table('symbols', self.getSchema())
+        assert not table.exists()
+        table.create()
+        assert table.exists()
 
 
     def delete(self):
-        table = self.client.dataset('downloader').table('symbols')
-        if table.exists():
-            table.delete()
+        dataset = self.client.dataset('downloader')
+        table = dataset.table('symbols', self.getSchema())
+        assert table.exists()
+        table.delete()
+        assert not table.exists()
 
 
     def upload(self, filename):
-        table = self.client.dataset('downloader').table('symbols')
-        table = self.setSchema(table)
+        dataset = self.client.dataset('downloader')
+        table = dataset.table('symbols', self.getSchema())
         with open(filename, 'rb') as readable:
             table.upload_from_file(readable, source_format='CSV')
 
@@ -47,7 +50,7 @@ class database():
         query_results = self.client.run_sync_query(query)
         query_results.use_legacy_sql = False
         query_results.run()
-
+        assert query.complete
         [rows, unused_total_rows, unused_page_token] = query_results.fetch_data()
         symbols = []
         for row in rows:
