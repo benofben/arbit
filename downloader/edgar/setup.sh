e6d5e3e429fad7141b74f96163192760586a737d
Ben Lackey <ben.lackey@outlook.com>
2018-04-15 15:09:19 -0700

    create table

--- downloader/edgar/setup.sh
+++ downloader/edgar/setup.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# Setup DynamoDB
+
+aws dynamodb create-table \
+  --attribute-definitions <value> \
+  --table-name downloader-edgar-form4-transactions \
+  --key-schema <value>
+
+# Setup Lambda
+
 cd day
 ./setup.sh
 cd ..
