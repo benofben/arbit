d07e43f064d833599d9d61d7ec12c7bbcec9a140
Ben Lackey <ben.lackey@outlook.com>
2018-05-21 12:50:18 -0700

    delete various AWS specific files

--- downloader/edgar/day/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-aws lambda invoke \
-  --invocation-type RequestResponse \
-  --function-name downloader-edgar-day \
-  --payload '{"date":"2018-04-02"}' \
-  output.txt
--- downloader/edgar/form4/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-aws lambda invoke \
-  --invocation-type RequestResponse \
-  --function-name downloader-edgar-form4 \
-  --payload '{"url":"https://www.sec.gov/Archives/edgar/data/1035927/0001664015-18-000004.txt"}' \
-  output.txt
