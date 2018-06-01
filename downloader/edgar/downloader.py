a91eed1f1e1b2a5fa268e27164d9503798793120
Ben Lackey <ben.lackey@outlook.com>
2018-05-31 20:43:03 -0700

    notes on download time

--- downloader/edgar/downloader.py
+++ downloader/edgar/downloader.py
@@ -1,15 +1,18 @@
 import sched
 import datetime
 import time
-
+import edgar
 
 class downloader:
     schedule = sched.scheduler(time.time, time.sleep)
 
+    # It looks like new master files show up at 2:01am, though are sometimes delayed as late as 2:14am.
+    # Something else is showing up at 10pm eastern, but it's unclear from my old comments.  Need to investigate.
+    # ... showing up at 10pm eastern.
+    # My old notes make no sense and contradict each other.  Ned to investigate...
+    downloadTime = datetime.time(22,30,0)
+
     def __init__(self):
-        # It looks like new master files show up at 2:01am, though are sometimes delayed as late as 2:14am.
-        # Something else is showing up at 10pm eastern, but it's unclear from my old comments.  Need to investigate.
-        downloadTime = datetime.time(22,30,0)
         today = datetime.date.today()
         downloadDateTime = datetime.datetime.combine(today, downloadTime)
         downloadTime = time.mktime(downloadDateTime.timetuple())
@@ -20,8 +23,7 @@ class downloader:
 
     def download(self):
         print('Running EDGAR download at ' + datetime.datetime.today().isoformat())
-        edgar.downloader.run()
-        #edgar.mail.run()
+        edgar.run()
         print('Done with EDGAR download at ' + datetime.datetime.today().isoformat())
 
         # Reschedule the download to run again tomorrow.
@@ -29,7 +31,6 @@ class downloader:
         today = datetime.date.today()
         tomorrow = today + datetime.timedelta(days=1)
 
-        downloadTime = constants.downloadtimeEDGAR
         downloadDateTime = datetime.datetime.combine(tomorrow, downloadTime)
         downloadTime = time.mktime(downloadDateTime.timetuple())
 
