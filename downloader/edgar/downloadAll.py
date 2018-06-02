22a95bea9075bdd90b6675e25b734fdf2f688428
Ben Lackey <ben.lackey@outlook.com>
2018-06-01 18:29:43 -0700

    bad format

--- downloader/edgar/downloadAll.py
+++ downloader/edgar/downloadAll.py
@@ -4,7 +4,7 @@ import edgar
 def run():
     date = datetime.date.today()
 
-    while date > datetime.date(1,1,2000):
+    while date > datetime.date(2000,12,31):
         date = date - datetime.timedelta(days=1)
         print(date)
         #edgar.downloadDate(date)
