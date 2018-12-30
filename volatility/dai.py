8c38c633c2e34270831e514af1958231434a81bc
Ben Lackey <ben.lackey@outlook.com>
2018-12-29 19:58:50 -0800

    trying preview

--- volatility/dai.py
+++ volatility/dai.py
@@ -6,33 +6,39 @@ h2oai = Client(address='http://129.213.63.69:12345', username='ben', password='b
 train_path = '/output.csv'
 train = h2oai.create_dataset_sync(train_path)
 
-exp_preview = h2oai.get_experiment_preview_sync(
+preview = h2oai.get_experiment_preview_sync(
     dataset_key=train.key,
     validset_key='',
+    dropped_cols=[],
     classification=True,
-    dropped_cols = [],
     target_col='LABEL',
     enable_gpus=True,
     accuracy=10,
     time=10,
     interpretability=1,
-    config_overrides=None,
-    is_time_series=True
+    is_time_series=True,
+    config_overrides='num_folds=1\n'
 )
 
-print(exp_preview)
+print(preview)
 
 experiment = h2oai.start_experiment_sync(
-    is_time_series=True,
     dataset_key = train.key,
-    testset_key = train.key,
     target_col = 'LABEL',
     is_classification = True,
+
     accuracy = 10,
     time = 10,
     interpretability = 1,
+
     scorer = "AUC",
-    seed = 1234
+    seed = 1234,
+
+    is_time_series=True,
+    num_folds=1,
+    num_gap_periods=1,
+    num_prediction_periods=1,
+    overlap = 0.000000
 )
 
 print("Final Model Score on Validation Data: " + str(round(experiment.valid_score, 3)))
