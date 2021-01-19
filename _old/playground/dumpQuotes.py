c82ba6ac3e15ff78ca0de6d1a0dfdf9a5d0e1011
Ben Lackey <ben.lackey@outlook.com>
2021-01-18 22:18:23 -0800

    drop old, edgar, etc.

--- _old/playground/dumpQuotes.py
+++
@@ -1,4 +0,0 @@
-import yahoo.database
-
-quoteDB = yahoo.database.database()
-quotes = quoteDB.writeQuotesToDisk()
--- _volatility/README.md
+++
@@ -1,1 +0,0 @@
-I can't figure out how to turn off cross validation.  It seems like this might be a bug in 1.4.2.
